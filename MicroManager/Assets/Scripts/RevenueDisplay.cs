e45a1092eb6b3d7557cc18142111d3b5e83e162e
Rohan Sinha <rohan.sinha16@gmail.com>
2020-07-28 22:28:59 -0700

    task managment scene with picture and changeable revenue

---
+++ MicroManager/Assets/Scripts/RevenueDisplay.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RevenueDisplay : MonoBehaviour
+{
+
+    private int revenue = 0;
+    public Text revenueText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        revenueText.text = GetRevenueText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        revenueText.text = GetRevenueText();
+        if(Input.GetMouseButtonUp(0))
+        {
+            CompleteTask();
+        }
+    }
+
+    private string GetRevenueText()
+    {
+        return "Revenue: $" + revenue;
+    }
+
+    public void CompleteTask()
+    {
+        revenue += 100;
+    }
+}
